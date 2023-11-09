bb3ff9a4e3b923e786e3fb00379735e8c563d92d
pelleon <progroman@gmail.com>
2023-11-09 17:36:14 +0600

    target change

--- main.tf
+++ main.tf
@@ -56,14 +56,14 @@ module "alb" {
   # Security Group
   security_groups = module.blog_sg.security_group_id
   
-  target_groups = [
+  target_groups = {
     ex-instance = {
       name_prefix      = "blog"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
     }
-  ]
+  }
 
   listeners = [
     {
